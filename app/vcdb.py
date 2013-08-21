9a61b62da1add35736e2e263fa6ed07eb1117bf2
Kevin Thompson <kevin.david.thompson@gmail.com>
2013-08-20 20:58:35 -0500

    created the app landing page and simple form to get started

--- app/vcdb.py
+++ app/vcdb.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 vcdb = Flask(__name__)
 
-@vcdb.route('/')
+@vcdb.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        options = ["theft_physical",
+                "tampering_physical",
+                "loss_error",
+                "publishing_error_error",
+                "misdelivery_error",
+                "disposal_error_error",
+                "privilege_abuse_misuse",
+                "embezzlement_misuse",
+                "dos_hacking",
+                "intrusion_hacking",
+                "malware_malware",
+                "long_form"]
+        if request.form['which_form'] in options:
+            return request.form['which_form']
+        else:
+            return "invalid form selection"
 
 if __name__ == "__main__":
     vcdb.run(debug=True, host="0.0.0.0")
