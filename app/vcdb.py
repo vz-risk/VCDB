f1ab067e6834eff6651084f1f2f73292052dcfec
Kevin Thompson <kevin.david.thompson@gmail.com>
2013-08-22 13:34:59 -0500

    adds routes for all the short forms and creates the start of a short form for physical theft.

--- app/vcdb.py
+++ app/vcdb.py
@@ -19,13 +19,62 @@ def index():
                 "malware_malware",
                 "long_form"]
         if request.form['which_form'] in options:
-            return request.form['which_form']
+            return redirect(url_for(request.form['which_form']))
         else:
             return "invalid form selection"
 
 @vcdb.route('/test')
 def test():
     return render_template('temp.html')
+
+@vcdb.route('/theft_physical')
+def theft_physical():
+    return render_template('theft_physical.html')
+
+@vcdb.route('/tampering_physical')
+def tampering_physical():
+    return "tampering physical"
+
+@vcdb.route('/loss_error')
+def loss_error():
+    return "loss error"
+
+@vcdb.route('/publishing_error_error')
+def publishing_error_error():
+    return "Publishing Error (Error)"
+
+@vcdb.route('/misdelivery_error')
+def misdelivery_error():
+    return "Misdelivery (Error)"
+
+@vcdb.route('/disposal_error_error')
+def disposal_error_error():
+    return "Disposal error (Error)"
+
+@vcdb.route('/privilege_abuse_misuse')
+def privilege_abuse_misuse():
+    return "Privilege abuse (Misuse)"
+
+@vcdb.route('/embezzlement_misuse')
+def embezzlement_misuse():
+    return "Embezzlement (Misuse)"
+
+@vcdb.route('/dos_hacking')
+def dos_hacking():
+    return "Denial of Service (Hacking)"
+
+@vcdb.route('/intrusion_hacking')
+def intrusion_hacking():
+    return "Intrusion (Hacking)"
+
+@vcdb.route('/malware_malware')
+def malware_malware():
+    return "Malware (Malware)"
+
+@vcdb.route('/long_form')
+def long_form():
+    return "Seriously? Do I really need to make a long form?"
+
 
 if __name__ == "__main__":
     vcdb.run(debug=True, host="0.0.0.0")
